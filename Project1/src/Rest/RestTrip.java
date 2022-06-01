18cacd0d90e171388ae1be6d0e35f4bbf988324f
Samir1113 <simo2000854@gmail.com>
2022-06-02 01:23:41 +0200

    Added create a trip API.

--- Project1/src/Rest/RestTrip.java
+++ Project1/src/Rest/RestTrip.java
@@ -1,7 +1,31 @@
 package Rest;
 
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import ejb.Trip;
+import services.Tripservice;
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("trip")
 public class RestTrip {
+	@EJB
+    private Tripservice tripService;
+	@POST
+	@Path("/{id}")
+	public String addtrip(Trip trip, @PathParam("id")int id)
+	{
+		return tripService.add(trip,id);
+		
+	}
 	
 }
--- Project1/src/services/Tripservice.java
+++ Project1/src/services/Tripservice.java
@@ -1,7 +1,31 @@
 package services;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import ejb.Trip;
+import ejb.User;
+
+@Stateless
 public class Tripservice {
+	@PersistenceContext
+    private EntityManager entityManager;
+	
+	public String add(Trip trip , int id) {
+			
+			User u =entityManager.find(User.class, id);
+			if(u.getRole().equals("admin"))
+			{
+				entityManager.persist(trip);
+				return "success";
+			}
+			else
+			{
+				return "false";
+			}
+	        
+	    }
 	
 
 }
