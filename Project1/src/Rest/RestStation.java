a6ce6cd06681bf2e78d556c4d7901e9f5e153ba4
MohamedTarek20196046 <mohamedtarek20196046@gmail.com>
2022-06-02 00:59:37 +0200

    addstation api

--- Project1/src/Rest/RestStation.java
+++ Project1/src/Rest/RestStation.java
@@ -1,7 +1,26 @@
 package Rest;
 
-
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import ejb.Station;
+import services.Stationservice;
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("station")
 public class RestStation {
-	
+	@EJB
+    private Stationservice stationService;
+	@POST
+	public String addstation(Station station)
+	{
+		stationService.add(station);
+		return "success";
+	}
 	
 }
--- Project1/src/services/Stationservice.java
+++ Project1/src/services/Stationservice.java
@@ -1,7 +1,20 @@
 package services;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import ejb.Station;
+
+
+@Stateless
 
 public class Stationservice {
 	
+	@PersistenceContext
+    private EntityManager entityManager;
+	
+	public void add(Station station) {
 
+        entityManager.persist(station);
+    }
 }
