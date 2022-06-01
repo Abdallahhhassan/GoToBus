142fe034793b306ce82e53ca3fc3c5002629694a
Samir1113 <simo2000854@gmail.com>
2022-06-01 22:36:59 +0200

    Added create user API.(1st)

--- Project1/src/Rest/RestUser.java
+++ Project1/src/Rest/RestUser.java
@@ -1,7 +1,47 @@
 package Rest;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 
+import ejb.User;
+import services.Userservice;
+
+import javax.ws.rs.POST;
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/user")
 public class RestUser {
+	@EJB
+    private Userservice userService;
+	@POST
+	
+	public String adduser(User user)
+	{
+		userService.add(user);
+		return "success";
+	}
+	
+	@GET
+	public List<User> getall()
+	{
+		return userService.getAllPersons();
+	}
+	@GET
+	@Path("/getID/{id}")
+	public User getID (@PathParam("id")int id)
+	{
+		return userService.findUserbyid(id);
+	}
 	
 	
 }
--- Project1/src/services/Userservice.java
+++ Project1/src/services/Userservice.java
@@ -1,5 +1,35 @@
 package services;
+import javax.ejb.Stateless;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+
+import ejb.User;
+
+@Stateless
 
 public class Userservice {
+	@PersistenceContext
+    private EntityManager entityManager;
+	
+	public void add(User user) {
+
+        entityManager.persist(user);
+    }
+
+	public List<User> getAllPersons() {
+		  TypedQuery<User> query = entityManager.createQuery("SELECT p FROM User p", User.class);
+		  	List<User> persons = query.getResultList();
+
+			  return persons;
+	}
+	
+	public User findUserbyid (int id)
+	{
+		return entityManager.find(User.class, id);
+	}
+	
 	
 }
